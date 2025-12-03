66b5db4e0972b68a845afb386053459d1e82ff9c
Jian-mnp <>
2025-12-03 17:04:31 +0700

    popup completed

--- popup/pop.js
+++ popup/pop.js
@@ -3,3 +3,9 @@ let close = document.querySelector(".close");
 let popup = document.querySelector(".info");
 
 btn.addEventListener("click", () => {
+    popup.parentElement.classList.add("show-modal");
+})
+
+close.addEventListener("click", () => {
+    popup.parentElement.classList.remove("show-modal");
+})
